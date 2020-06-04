9bb628c408babce19275c3507c05ee5b2966cfe5
tyq <731816192@qq.com>
2020-06-04 15:29:41 +0800

    add a test

---
+++ src/main/java/com/tyq/util/SmsUtil.java
@@ -0,0 +1,49 @@
+package com.tyq.util;
+
+import com.alibaba.fastjson.JSONObject;
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+
+import java.util.Map;
+
+public class SmsUtil {
+    public boolean sendMsm(String phone, Map map){
+
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4GDkTMgLW6CbpaUgApWp", "n0Wx7X9lbWQ0B0DgPKXdc0KS1lOwx3");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+
+
+        request.putQueryParameter("PhoneNumbers",phone);
+        request.putQueryParameter("SignName","OSCO");
+        request.putQueryParameter("TemplateCode","SMS_192370366");
+
+
+
+        request.putQueryParameter("TemplateParam", JSONObject.toJSONString(map));
+
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+            return false;
+        } catch (ClientException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+
+    }
+}
